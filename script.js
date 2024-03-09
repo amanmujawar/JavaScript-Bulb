a009bae8091296627bf47a6e0359314e3ce2c02a
Aman <amanmujawar9977@gmail.com>
2024-03-09 16:43:45 +0530

    Changes

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
     btn.addEventListener("click", function(){
         if (flag==0) {
             bulb.style.backgroundColor = "yellow"
-            console.log("clicked")
+            console.log("clicked bulb")
             flag=1
         }else{
             bulb.style.backgroundColor = "transparent"
-            console.log("Again clicked")
+            console.log("Again clicked bulb")
             flag = 0
         }
     })
